d75f96eba20632c2b26909cde4473214d03172c1
Rick Bassham <brodrick.bassham@gmail.com>
2022-05-13 13:12:06 -0400

    working monorepo

---
+++ packages/common/common/utils.py
@@ -0,0 +1,6 @@
+def say_hello():
+    print("hello2")
+
+
+def say_something():
+    print("something2")
---
+++ services/svc1/svc1/app.py
@@ -0,0 +1,4 @@
+from common.utils import say_hello, say_something
+
+say_hello()
+say_something()
